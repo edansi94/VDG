096a6dd5da8f073657e63a9a917d2672d6f57a9a
Christian Oliveros <christianol_01@hotmail.com>
2016-07-23 16:23:46 -0400

    Sistema para Interactuar y Puertas

---
+++ Assets/Scripts/Objects/I_Interactuable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface I_Interactuable {
+    // Si esta encendido
+    bool on { get; }
+    // Si solo puede ser interactuado una vez
+    bool oneWay { get; set; }
+    // Si fue interactuado, para oneWay
+    bool interactuado { get; set; }
+    // Respeta si es oneway
+    void Interact();
+    // No respeta si es oneway
+    void SuperInteract();
+}
---
+++ Assets/Scripts/Player/Interact.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Interact : MonoBehaviour {
+
+    private HashSet<I_Interactuable> inscritos = new HashSet<I_Interactuable>();
+
+    private bool presiono = false;
+
+
+    
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Interact")) {
+            presiono = true;
+        }
+	}
+
+    void FixedUpdate() {
+        if (presiono)
+        {
+            foreach (I_Interactuable inscrito in inscritos)
+            {
+                inscrito.Interact();
+            }
+            presiono = false;
+        }
+    }
+
+    public void inscribirse(I_Interactuable objeto) {
+        inscritos.Add(objeto);
+    }
+
+    public void salir(I_Interactuable objeto) {
+        inscritos.Remove(objeto);
+    }
+}
