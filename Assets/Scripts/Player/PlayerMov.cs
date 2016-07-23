8b41781aed64fa069c49a6ed92b845b3ffff013f
Christian Oliveros <christianol_01@hotmail.com>
2016-07-23 01:25:14 -0400

    Movimiento Player

---
+++ Assets/Scripts/Player/PlayerMov.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMov : MonoBehaviour {
+
+    [SerializeField]
+    string inputNameHor = "Horizontal";
+
+    [SerializeField]
+    const string inputNameVer = "Vertical";
+
+    [SerializeField]
+    float fuerza = 5f;
+
+
+    private Rigidbody2D body;
+    private Vector2 dir;
+
+    // Use this for initialization
+    void Start () {
+        body = GetComponent<Rigidbody2D>();
+    }
+	
+	// Fixed Update is called once per physics tick
+	void FixedUpdate () {
+        dir = new Vector2(Input.GetAxis(inputNameHor), Input.GetAxis(inputNameVer));
+        dir = dir.normalized * fuerza;
+        if (dir != Vector2.zero)
+        {
+            body.AddForce(dir);
+        }
+	}
+}
