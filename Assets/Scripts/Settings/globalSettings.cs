a37d590d7e7ea96ae67f72ec10c627653abe20ca
Edward <edwardfernande26@gmail.com>
2016-07-23 16:45:56 -0400

    Cuando tocamos algo que se considere alma nos aumenta la cantidad de almas y se destruye.

---
+++ Assets/Scripts/Settings/globalSettings.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class globalSettings : MonoBehaviour {
+
+	[Serializable]
+	public class playerSettings
+	{
+		public int soulAmount = 0; // This will be the amount of souls we have.
+		public int energy = 100;    // This will be the player's life.
+
+		public int SoulAmount
+		{
+			get { return soulAmount; }
+		}
+
+		public void setSoulAmount(int value)
+		{
+			soulAmount += value;
+		}
+
+
+		public int Energy
+		{
+			get { return energy; }
+		}
+
+		public void setEnergy(int value)
+		{
+			energy += value;
+		}
+	}
+
+	public playerSettings PlayerSettings;
+}
