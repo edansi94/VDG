14e59567c2471091df8fb1b05ec7f5f66af95dc1
Christian Oliveros <christianol_01@hotmail.com>
2016-07-23 17:27:53 -0400

    Bombillo

--- Assets/Scripts/Objects/Bombillo.cs
+++ Assets/Scripts/Objects/Bombillo.cs
@@ -1,15 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Bombillo : MonoBehaviour {
+public class Bombillo : MonoBehaviour, I_Interactuable {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    [SerializeField]
+    bool oneWay = false;
+
+    [SerializeField]
+    bool encendido = false;
+
+    bool I_Interactuable.on { get { return encendido; } }
+
+    bool I_Interactuable.oneWay { get { return oneWay; } set { oneWay = value; } }
+
+    bool interactuado = false;
+    bool I_Interactuable.interactuado { get { return interactuado; } set { interactuado = value; } }
+
+    private Animator anim;
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        if (encendido)
+        {
+            anim.SetTrigger("Interactuar");
+        }
+    }
+
+    void I_Interactuable.Interact()
+    {
+        if (!oneWay || !interactuado)
+        {
+            ((I_Interactuable)this).SuperInteract();
+        }
+    }
+
+    void I_Interactuable.SuperInteract()
+    {
+        encendido = !encendido;
+        anim.SetTrigger("Interactuar");
+        interactuado = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<Interact>().inscribirse((I_Interactuable)this);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<Interact>().salir((I_Interactuable)this);
+        }
+    }
 }
