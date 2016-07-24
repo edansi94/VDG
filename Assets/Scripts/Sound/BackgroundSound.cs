4bcc97452ef59d4213d6f2c7f7086a940e73331a
Christian Oliveros <christianol_01@hotmail.com>
2016-07-24 19:38:26 -0400

    Sonido Intro

--- Assets/Scripts/Sound/BackgroundSound.cs
+++ Assets/Scripts/Sound/BackgroundSound.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class BackgroundSound : MonoBehaviour {
     AudioSource fxSound; 
     [SerializeField]
-    AudioClip backMusic; 
+    AudioClip backMusic;
+    [SerializeField]
+    AudioClip introMusic;
+
     void Start()
     {
         fxSound = GetComponent<AudioSource>();
-        fxSound.clip = backMusic;
+        fxSound.clip = introMusic;
         fxSound.Play();
+    }
+
+    void OnLevelWasLoaded(int level)
+    {
+        if (level != SceneManager.GetSceneByName("Inicio").buildIndex)
+        {
+            if (fxSound.clip != backMusic)
+            {
+                fxSound.clip = backMusic;
+                fxSound.Play();
+            }
+        }
+        else
+        {
+            fxSound.clip = introMusic;
+            fxSound.Play();
+        }
+        
     }
 
     void Awake()
