c499412249a25a94f6b03e2f94f4e6216784eb8f
Christian Oliveros <christianol_01@hotmail.com>
2016-07-24 15:54:19 -0400

    Sistema de Musica de Fondo

---
+++ Assets/Scripts/Sound/BackgroundSound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundSound : MonoBehaviour {
+    AudioSource fxSound; 
+    [SerializeField]
+    AudioClip backMusic; 
+    void Start()
+    {
+        fxSound = GetComponent<AudioSource>();
+        fxSound.clip = backMusic;
+        fxSound.Play();
+    }
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+}
