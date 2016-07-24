48ff9440a0bc696ba0db6458929790cf857d157d
Christian Oliveros <christianol_01@hotmail.com>
2016-07-23 23:18:08 -0400

    Sistema de Carga de Niveles y Pantallas de Carga

---
+++ Assets/Scripts/Settings/memoriesManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class memoriesManager : MonoBehaviour {
+
+    [SerializeField]
+    string loadingScene;
+
+    // levelScene[4] == Scene final donde se ve recuerdos tomados
+    [SerializeField]
+    string[] levelScene = new string[5];
+
+    [SerializeField]
+    int level = 0;
+
+    int[] chosenMemories = new int[4];
+	// Use this for initialization
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+
+    public void elegirRecuerdo(int noRecuerdo) {
+        if (level < 5)
+        {
+            chosenMemories[level] = noRecuerdo;
+            level++;
+            SceneManager.LoadScene(loadingScene);
+        }
+    }
+
+    public string sceneToLoad() {
+        return levelScene[level];
+    }
+}
